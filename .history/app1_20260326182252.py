98457bc54307ab641fa609cc124ff23b49d55b68
Pacott1 <f.delascuevasvalencia@gmail.com>
2026-03-28 12:47:38 +0100

    add files

---
+++ .history/app1_20260326182252.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config["DEBUG"] = True # modo de depuracion
+
+@app.route('/', methods=['GET'])
+def home():
+    return """
+    <h1>The Bridge Distant Reading Archive</h1>
+    <p>This site is a prototype API for distant reading of science fiction novels.</p>
+    """
+
+app.run()   
